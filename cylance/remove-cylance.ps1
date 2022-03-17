af62443522ad55fd8b9496e988098ea6788442ef
bjonescts <70379785+bjonescts@users.noreply.github.com>
2022-03-17 08:40:07 -0400

    Update remove-cylance.ps1

--- cylance/remove-cylance.ps1
+++ cylance/remove-cylance.ps1
@@ -7,18 +7,17 @@ mkdir $cremoval
 #Download psexec and extract
 Invoke-WebRequest -Uri $source -OutFile $destination
 Expand-Archive -LiteralPath $destination -DestinationPath $cremoval
-#Reset Variables
-$source = 'https://helgeklein.com/downloads/SetACL/current/SetACL 3.1.2 (executable version).zip'
-$destination = '\tmp\c-removal\SetACL.zip'
+#Reset Variables and download SetACL from github
+$source = 'https://github.com/bjonescts/CTS_Powershell_Scripts/raw/main/cylance/tools/SetACL.exe'
+$destination = '\tmp\c-removal\SetACL.exe'
 Invoke-WebRequest -Uri $source -OutFile $destination
-Expand-Archive -LiteralPath $destination -DestinationPath $cremoval
 #Execute psexec to disable the Cylance service from starting
 $psexec = '\tmp\c-removal\PsExec.exe'
 Invoke-Expression -Command "$psexec -accepteula -h -s sc config cylancesvc start= disabled"
 #Reboot
 Restart-Computer -Wait -Force
 #Modify the protected registry keys
-$setacl = '\tmp\c-removal\SetACL (executable version)\64 bit\SetACL.exe'
+$setacl = '\tmp\c-removal\SetACL.exe'
 Invoke-Expression -Command "$setacl -on 'HKLM\SOFTWARE\Cylance\Desktop' -ot reg -actn setowner -ownr 'n:Administrators'"
 Invoke-Expression -Command "$setacl -on 'HKLM\SOFTWARE\Cylance\Desktop' -ot reg -actn ace -ace n:Administrators;p:full"
 Set-ItemProperty 'HKLM:\SOFTWARE\Cylance\Desktop' -Name 'SelfProtectionLevel' -Value 1
